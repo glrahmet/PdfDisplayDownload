fdd4e2d8aeb534bd926866182f9dfae60e935a7f
Güler Ahmet <glrahmt@outlook.com>
2018-02-25 11:31:07 +0300

    get init

---
+++ PdfDisplayDownLoad/PdfDisplayDownLoad/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using PdfDisplayDownLoad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PdfDisplayDownLoad.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileResult PDFDisplay()
+        {
+            string filepath = Server.MapPath("/Document/ReadMe.pdf");
+            byte[] pdfByte = Helper.GetBytesFromFile(filepath);
+            return File(pdfByte, "application/pdf");
+        }
+        public FileResult PDFDownload()
+        { 
+            string filepath = Server.MapPath("/Document/ReadMe.pdf");
+            byte[] pdfByte = Helper.GetBytesFromFile(filepath);
+            return File(pdfByte, "application/pdf", "ReadMe.pdf");
+        }
+    }
+}
---
+++ PdfDisplayDownLoad/PdfDisplayDownLoad/Models/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PdfDisplayDownLoad.Models
+{
+    public static class Helper
+    { 
+        public static byte[] GetBytesFromFile(string fullFilePath)
+        { 
+            FileStream fs = null;
+            try
+            {
+                fs = File.OpenRead(fullFilePath);
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, Convert.ToInt32(fs.Length));
+                return bytes;
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                    fs.Dispose();
+                }
+            } 
+        }
+    }
+}
